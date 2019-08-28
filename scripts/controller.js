3a95d7d939368bbe4fe1a383aa650af9b8b0769e
unknown <2ndyrGroupA@PN16.pn.local>
2019-08-28 10:28:48 +0800

    added buttons with event listener

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,18 +1,30 @@
+var btn_connect = document.getElementById('btn-connect');
+var btn_publish = document.getElementById('btn-publish');
+
+btn_connect.addEventListener('click', function(e){
+  e.preventDefault();
+  console.log("connect button")
+
+  client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
+  client.subscribe("mqtt/demos")
+
+  client.on("connect", function(){
+      console.log("Successfully connected");
+  })
+
+  client.on("message", function (topic, payload) {
+    console.log([topic, payload].join(": "));
+    // client.end();
+  })
+})
+
+btn_publish.addEventListener('click', function(event){
+  event.preventDefault();
+  console.log("publish button");
+  client.publish("mqtt/demos", "hello world!")
+})
+
 // basic functionalities
-client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
-client.subscribe("mqtt/demo")
-
-client.on("connect", function(){
-    console.log("Successfully connected");
-})
-
-client.on("message", function (topic, payload) {
-  console.log([topic, payload].join(": "));
-  client.end();
-})
-
-client.publish("mqtt/demo", "hello world!")
-
 // // advance functionalities
 // client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
 // client.subscribe("mqtt/demo", function (err){

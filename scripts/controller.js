b88d1d93569c718d5c31929ea25dfce27ea7fc4f
binondoci_sd2081@CEACCS.NET <binondoci_sd2081@CEACCS.NET>
2019-09-09 19:39:12 -0600

    modify script & style and add bootstrap

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,20 +1,21 @@
 funcConnect = () => {
-    console.log("Connecting..");
-    client = mqtt.connect(document.getElementById('broker').value)
-    console.log(document.getElementById('broker').value);
+  console.log("Connecting..");
+  client = mqtt.connect(document.getElementById('broker').value)
+  console.log(document.getElementById('broker').value);
 
-    client.on("connect", function(){
-      console.log("Successfully connected");
-    })
-    client.on("message", function (topic, payload) {
-      console.log("Received { topic: " + topic + "; payload: " + payload + " } ");
-})
-funcPublish = () => {
-  client.publish(document.getElementById('pub-topic').value, document.getElementById('pub-payload').value)
-  console.log("Published { topic:" + document.getElementById('pub-topic').value +
-  ";payload: " + document.getElementById('pub-payload').value + "}");
+  client.on("connect", function(){
+    console.log("Successfully connected");
+  })
+  client.on("message", function (topic, payload) {
+    console.log("Received { topic: " + topic + "; payload: " + payload + " } ");
+  })
+  funcPublish = () => {
+    client.publish(document.getElementById('pub-topic').value, document.getElementById('pub-payload').value)
+    console.log("Published { topic:" + document.getElementById('pub-topic').value +
+    ";payload: " + document.getElementById('pub-payload').value + "}");
+  }
+  funcSubscribe = () => {
+    client.subscribe(document.getElementById('sub-topic').value);
+    console.log("Subscribe { topic: " + document.getElementById('sub-topic').value + "}");
+  }
 }
-funcSubscribe = () => {
-  client.subscribe(document.getElementById('sub-topic').value);
-  console.log("Subscribe { topic: " + document.getElementById('sub-topic').value + "}");
-}}
